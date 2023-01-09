6b92e6f4a525d61826f8ea4cca3b8355cde5f32d
HosamSaleh1 <mr.hosamsaleh@yahoo.com>
2023-01-09 23:56:00 +0200

    finishing the team process

---
+++ app/Http/Controllers/TeamController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Player;
+use Illuminate\Http\Request;
+
+use function PHPSTORM_META\type;
+
+class TeamController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(Request $request)
+    {
+        $allPlayers = [];
+        for ($i=0; $i < count($request->all()); $i++) { 
+                // $players = Player::where('position', $request->all()[$i]['position'])->limit($request->all()[$i]['numberOfPlayers'])->get();
+                $players = Player::where('position', $request->all()[$i]['position'])
+                                    // ->skills()->max($request->all()[$i]['mainSkill'])
+                                    ->limit($request->all()[$i]['numberOfPlayers'])->get();
+
+                if(!$players || count($players) < $request->all()[$i]['numberOfPlayers']){
+                    return response()->json(['message' => 'Insufficient number of players for position: '. $request->all()[$i]['position']], 404);
+                }
+                $allPlayers = array_merge($allPlayers, $players->toArray());
+            }
+        
+        return response()->json($allPlayers);
+    }
+}
